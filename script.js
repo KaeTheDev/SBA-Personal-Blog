aba9ea5da6ba1ba054db573f3e1079053992eb6e
KaeTheDev <kaethedev@gmail.com>
2025-11-07 11:43:11 -0500

    Created addBlog function with push to blogs array in localStorage

--- script.js
+++ script.js
@@ -4,3 +4,21 @@ const addBlogBtn = document.getElementById("addBlogBtn");
 const blogList = document.getElementById("blogList"); 
 
 let blogs = JSON.parse(localStorage.getItem("blogs")) || [];
+
+// Calls to Event Listeners
+addBlogBtn.addEventListener("click", addBlog)
+
+function addBlog(){
+    let blog = {
+        blogTitle: blogTitle.value,
+        blogContent: blogContent.value,
+        timeStamp: Date.now()
+    };
+
+    blogs.push(blog);
+    localStorage.setItem("blogs", JSON.stringify(blogs));
+
+
+    blogTitle.value = '';
+    blogContent.value = '';
+}
